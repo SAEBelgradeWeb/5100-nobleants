9812208d5cc97c820b7768b70771692e64f683ae
Milos <miloske1104@gmail.com>
2018-05-14 19:54:33 +0200

    Done Homework

---
+++ Homework/task5.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Task 5</title>
+</head>
+<body>
+<?php
+    $first_name = "Harry";
+    var_dump($first_name);
+    echo "<p>";
+    print $first_name;
+    echo "<p>";
+    $number = 28;
+    var_dump($number);
+    echo "<p>";
+    $null_var = null;
+    var_dump($null_var);
+?>
+</body>
+</html>
